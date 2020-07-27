d88945632a586dfe6c6ef7e46e00fc569f367fdd
EduardoCeolin <eduardo_ceolin@hotmail.com>
2020-07-27 19:13:00 -0300

    Animacoes adicionadas

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,47 +1,56 @@
-import React from 'react';
-import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styles';
-import VideoCard from './components/VideoCard';
+import React from "react";
+import {
+  VideoCardGroupContainer,
+  VideoCardList,
+  Title,
+  ExtraLink,
+} from "./styles";
+import VideoCard from "./components/VideoCard";
 
-function VideoCardGroup({
-  ignoreFirstVideo,
-  category,
-}) {
+import ScrollAnimation from "react-animate-on-scroll";
+
+function VideoCardGroup({ ignoreFirstVideo, category }) {
   const categoryTitle = category.titulo;
   const categoryColor = category.cor;
   const categoryExtraLink = category.link_extra;
   const videos = category.videos;
-  return (
-    <VideoCardGroupContainer>
-      {categoryTitle && (
-        <>
-          <Title style={{ backgroundColor: categoryColor || 'red' }}>
-            {categoryTitle}
-          </Title>
-          {categoryExtraLink && 
-            <ExtraLink href={categoryExtraLink.url} target="_blank">
-              {categoryExtraLink.text}  
-            </ExtraLink>
-          }
-        </>
-      )}
-      <VideoCardList>
-        {videos.map((video, index) => {
-          if (ignoreFirstVideo && index === 0) {
-            return null;
-          }
 
-          return (
-            <li key={video.titulo}>
-              <VideoCard
-                videoTitle={video.titulo}
-                videoURL={video.url}
-                categoryColor={categoryColor}
-              />
-            </li>
-          );
-        })}
-      </VideoCardList>
-    </VideoCardGroupContainer>
+  return (
+    <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" delay={100}>
+      <VideoCardGroupContainer>
+        {categoryTitle && (
+          <>
+            <Title style={{ backgroundColor: categoryColor || "red" }}>
+              {categoryTitle}
+            </Title>
+            {categoryExtraLink && (
+              <ExtraLink href={categoryExtraLink.url} target="_blank">
+                {categoryExtraLink.text}
+              </ExtraLink>
+            )}
+          </>
+        )}
+        <VideoCardList>
+          {videos.map((video, index) => {
+            if (ignoreFirstVideo && index === 0) {
+              return null;
+            }
+
+            return (
+              <li key={video.titulo}>
+                {
+                  <VideoCard
+                    videoTitle={video.titulo}
+                    videoURL={video.url}
+                    categoryColor={categoryColor}
+                  />
+                }
+              </li>
+            );
+          })}
+        </VideoCardList>
+      </VideoCardGroupContainer>
+    </ScrollAnimation>
   );
 }
 
