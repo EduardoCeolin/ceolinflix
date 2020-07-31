03db2f1f0bc2a9896e2c8c0b5656802858aa2eeb
EduardoCeolin <eduardo_ceolin@hotmail.com>
2020-07-31 13:34:33 -0300

    url ajustada

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -27,7 +27,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://ceolinflix.herokuapp.com/categorias';
 
     fetch(URL).then(async (response) => {
       setCategorias([...(await response.json())]);
